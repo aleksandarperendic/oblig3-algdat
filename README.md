14ed81bde4db1155a4f9beb11588691ce34947af
Aleksandar Perendic <70316883+aleksandarperendic@users.noreply.github.com>
2021-10-28 16:23:49 +0200

    Ordnet oppgave 4, feil med rekkefølgen. Oppdatert README.md

--- README.md
+++ README.md
@@ -7,10 +7,16 @@ Oppgaven er levert av følgende student:
 
 # Oppgavebeskrivelse
 
-I oppgave 1 så gikk jeg frem ved å kopiere leggInn fra kompendiet, og så adde "q" på riktig plass.
+**I oppgave 1** så gikk jeg frem ved å kopiere leggInn fra kompendiet, og så adde "q" på riktig plass.
 
-Oppgave 2 løste jeg ved hjelp av kompendiet og comparator. Definerte en int som blir enten -1, 0 eller 1 når den går gjennom alle tall. Oppdaterte antall når int cmp var lik 0, altså når den fant samme tall i treet vi søkte med antall. 
+**Oppgave 2** løste jeg ved hjelp av kompendiet og comparator. Definerte en int som blir enten -1, 0 eller 1 når den går gjennom alle tall. Oppdaterte antall når int cmp var lik 0, altså når den fant samme tall i treet vi søkte med antall. 
 
-I oppgave 3 gikk jeg frem ved å bruke teori fra kompendiet, og så forsøke å kode det på java. Utfordring var å skjønne hvordan man finner nestPostorden. 
+**I oppgave 3** gikk jeg frem ved å bruke teori fra kompendiet, og så forsøke å kode det på java. Det som hjalp mest var jo da "Den siste i preorden er lik den første i speilvendt postorden.". Utfordring var å skjønne hvordan man finner nestPostorden, og da tegnet jeg selv et par trær for å se hvor blir det andre elementet i postorder. 
 
-I oppgave 4 har jeg bare endret oppgave 3 og brukte rekursjon. Ikke ferdig ennå.
+**Oppgave 4** startet jeg med å bruke en del av oppgave 3. _Iterativ metode_ kalte inn førstePostorden og så i while-løkken kalte den nestePostorden. For _rekursjon_ var det ganske likt som forrige oppgaven, bare at metoden kalte seg selv fra venstre/høyre side. 
+
+**Oppgave 5** fikk jeg help fra siste forelesningsvideo i uke 42, bare endre rekkefølgen slik at den oppfyller krav fra oppgaven. Oppretter en liste og en ArrayDeque, gikk gjennom treet og satt elementer på riktig plass i arrayet. Altså nivåorden.
+
+**I oppgave 6** kopierte jeg metoden fra kompendiet, som forklart i oppgaven. Videre brukte jeg en while-løkke på fjernAlle() og på nullstill() bruke jeg stack til å pushe/pope som forklart i forelesningen. Til slutt ble alt satt på 'null' og antall = 0.
+
+**Alle oppgaver består testen jeg har laget selv, og _Oblig3Test_.**
--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -80,45 +80,57 @@ public class SBinTre<T> {
         return antall == 0;
     }
 
+    // OPPGAVE 1 - hjelp fra kompendiet
     public boolean leggInn(T verdi) {
         Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
 
-        Node<T> p = rot, q = null;               // p starter i roten
-        int cmp = 0;                             // hjelpevariabel
+        Node<T> p = rot, q = null;                      // p starter i roten
+        int cmp = 0;                                    // hjelpevariabel
         while (p != null){
-            q = p;                                 // q er forelder til p
-            cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
-            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+            q = p;                                      // q er forelder til p
+            cmp = comp.compare(verdi,p.verdi);          // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre;          // flytter p
         }
+        // Oppdatert til å ta inn 'verdi' og 'p'
         p = new Node<>(verdi, q);
 
-        if (q == null) rot = p;                  // p blir rotnode
-        else if (cmp < 0) q.venstre = p;         // venstre barn til q
-        else q.høyre = p;                        // høyre barn til q
+        if (q == null) rot = p;                         // p blir rotnode
+        else if (cmp < 0) q.venstre = p;                // venstre barn til q
+        else q.høyre = p;                               // høyre barn til q
 
         antall++;
-        return true;                             // vellykket innlegging
+        return true;                                    // vellykket innlegging
     }
 
-    public boolean fjern(T verdi) {
 
-        if (verdi == null) return false;
-        Node<T> p = rot, q = null;
-        while (p != null) {
-            int cmp = comp.compare(verdi,p.verdi);
-            if (cmp < 0) { q = p; p = p.venstre; }
-            else if (cmp > 0) { q = p; p = p.høyre; }
-            else break;
+    // OPPGAVE 6
+    public boolean fjern(T verdi) {                     // hører til klassen SBinTre
+
+        if (verdi == null) {                            // treet har ingen nullverdier
+            return false;
         }
 
-        if (p == null) return false;
-        if (p.venstre == null || p.høyre == null){
-            Node<T> b;
+        Node<T> p = rot, q = null;                      // q skal være forelder til p
+
+        while (p != null) {                             // leter etter verdi
+            int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+            if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+            else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+            else break;                                 // den søkte verdien ligger i p
+        }
+
+        if (p == null) {                                // finner ikke verdi
+            return false;
+        }
+
+        if (p.venstre == null || p.høyre == null){      // Tilfelle 1) og 2)
+            Node<T> b;                                  // b for barn
             if(p.venstre != null) {
                 b = p.venstre;
             } else {
                 b = p.høyre;
             }
+            // Sjekker om b ikke er 'null'
             if (b != null) {
                 b.forelder = q;
             }
@@ -131,14 +143,15 @@ public class SBinTre<T> {
             else q.høyre = b;
         }
 
-        else {
-            Node<T> s = p, r = p.høyre;
+        else {                                          // Tilfelle 3)
+            Node<T> s = p, r = p.høyre;                 // finner neste i inorden
             while (r.venstre != null) {
-                s = r;
+                s = r;                                  // s er forelder til r
                 r = r.venstre;
             }
-            p.verdi = r.verdi;
+            p.verdi = r.verdi;                          // kopierer verdien i r til p
 
+            // Sjekke om r sin høyre ikke er 'null'
             if (r.høyre != null) {
                 r.høyre.forelder = s;
             }
@@ -151,18 +164,55 @@ public class SBinTre<T> {
             }
         }
 
-        antall--;
+        antall--;                                       // det er nå én node mindre i treet
         return true;
     }
 
+
     public int fjernAlle(T verdi) {
         int antall = 0;
+        // While-løkke som kaller inn fjern()
         while (fjern(verdi)) {
             antall++;
         }
         return antall;
     }
 
+
+    public void nullstill() {
+
+        if (rot == null) {
+            return;
+        }
+
+        // Bruke stack til å pushe/pope som forklart i forelesningen
+        ArrayList<Node> liste = new ArrayList();
+        ArrayDeque<Node> stack = new ArrayDeque<>();
+
+        while (rot != null || stack.size() > 0) {
+            while (rot != null) {
+                stack.push(rot);
+                rot = rot.høyre;
+            }
+            rot = stack.pop();
+            liste.add(rot);
+            rot = rot.venstre;
+        }
+
+        // Nullstiller alle elementer
+        for (Node node: liste) {
+            node.forelder = null;
+            node.venstre = null;
+            node.høyre = null;
+            node.verdi = null;
+        }
+
+        // Setter antall på 0
+        antall = 0;
+    }
+
+
+    // OPPGAVE 2
     public int antall(T verdi) {
        // throw new UnsupportedOperationException("Ikke kodet ennå!");
 
@@ -196,42 +246,19 @@ public class SBinTre<T> {
         return antall;
     }
 
-    public void nullstill() {
 
-        if (rot == null) {
-            return;
-        }
-
-        ArrayList<Node> liste = new ArrayList();
-        ArrayDeque<Node> stack = new ArrayDeque<>();
-
-        while (rot != null || stack.size() > 0) {
-            while (rot != null) {
-                stack.push(rot);
-                rot = rot.høyre;
-            }
-            rot = stack.pop();
-            liste.add(rot);
-            rot = rot.venstre;
-        }
-
-        for(Node node: liste) {
-            node.forelder = null;
-            node.venstre = null;
-            node.høyre = null;
-            node.verdi = null;
-        }
-
-        antall = 0;
-    }
-
+    // OPPGAVE 3
     private static <T> Node<T> førstePostorden(Node<T> p) {
+
         // Fra kompendiet 5.1.7
         // -> "Den siste i preorden er lik den første i speilvendt postorden."
-
         while (true) {
-            if (p.venstre != null) p = p.venstre;
-            else if (p.høyre != null) p = p.høyre;
+            if (p.venstre != null) {
+                p = p.venstre;
+            }
+            else if (p.høyre != null) {
+                p = p.høyre;
+            }
             else return p;
         }
     }
@@ -254,14 +281,15 @@ public class SBinTre<T> {
         return p.forelder;
     }
 
+    // OPPGAVE 4
     public void postorden(Oppgave<? super T> oppgave) {
-        // throw new UnsupportedOperationException("Ikke kodet ennå!");
+        // Løser oppgaven som forklart i oppgaveteksten. Kaller inn førstePostorden, og så
+        // i while-løkke setter man p til å være nestePosorden(p)
         Node<T> p = førstePostorden(rot);
         while (p != null) {
+            oppgave.utførOppgave(p.verdi);
             p = nestePostorden(p);
         }
-        oppgave.utførOppgave(p.verdi);
-
     }
 
     public void postordenRecursive(Oppgave<? super T> oppgave) {
@@ -269,9 +297,8 @@ public class SBinTre<T> {
     }
 
     private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
-       // throw new UnsupportedOperationException("Ikke kodet ennå!");
-
-        // Bruk av rekursjon til å løse oppgaven. Ganske likt forrige eksemplet
+        // Bruk av rekursjon til å løse oppgaven. Ganske likt forrige eksemplet,
+        // bare at man kaller seg selv venstre/høyre side så lenge det ikke er null
         if(p.venstre != null) {
             postordenRecursive(p.venstre, oppgave);
         } if(p.høyre != null) {
@@ -280,27 +307,12 @@ public class SBinTre<T> {
         oppgave.utførOppgave(p.verdi);
     }
 
+
+    // OPPGAVE 5
     public ArrayList<T> serialize() {
-
-        // LØSTE OPPGAVE UTEN Å SE ORDENTLIG PÅ OPPGAVETEKSEN. DER STO DET NEMLIG
-        // AT MAN IKKE KAN BRUKE REKURSJON.
-
-      /*  ArrayList<T> data = new ArrayList<>();
-        serializeHjelper(rot, data);
-        return data;
-    }
-
-    private void serializeHjelper(Node verdi, ArrayList<T> data){
-        if (verdi == null) {
-            return;
-        }
-        data.add(rot.verdi);
-            serializeHjelper(verdi.venstre, data);
-            serializeHjelper(verdi.høyre, data);
-*/
-
-        // Iterativ
-
+        // Iterativ metode, lignende som i siste video fra uke 42 av forelesning.
+        // Oppretter en liste og en ArrayDeque, går gjennom treet og setter
+        // elementer på riktig plass i arrayet. Altså nivåorden.
         ArrayList<T> liste = new ArrayList<>();
         ArrayDeque<Node<T>> queue = new ArrayDeque<>();
 
@@ -320,14 +332,32 @@ public class SBinTre<T> {
             liste.add(curr.verdi);
         }
 
-        //return new ArrayList<>(liste);
+        return new ArrayList<>(liste);
+    }
+
+    // LØSTE OPPGAVE OGSÅ UTEN Å SE ORDENTLIG PÅ OPPGAVETEKSEN. DER STO DET NEMLIG
+    // AT MAN IKKE KAN BRUKE REKURSJON.
+
+      /*  ArrayList<T> liste = new ArrayList<>();
+        serializeHjelper(rot, liste);
         return liste;
     }
 
+    private void serializeHjelper(Node verdi, ArrayList<T> liste){
+        if (verdi == null) {
+            return;
+        }
+        data.add(rot.verdi);
+            serializeHjelper(verdi.venstre, liste);
+            serializeHjelper(verdi.høyre, liste);
+*/
+
     static <K> SBinTre<K> deserialize(ArrayList<K> data, Comparator<? super K> c) {
-        // throw new UnsupportedOperationException("Ikke kodet ennå!");
+        // Oppdatter en liste med verdier fra data og et tre
         ArrayList<K> liste = new ArrayList<>(data);
         SBinTre<K> tre = new SBinTre<>(c);
+        // Bruker for-løkke til å plassere elementene fra arrayet til treet
+        // ved hjelp av leggInn fra oppgave 1
         for (K verdi : liste) {
             tre.leggInn(verdi);
         }
